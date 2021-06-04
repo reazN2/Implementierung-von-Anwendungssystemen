ac8a25b549aeb4a3a9598197465687046f2780b7
Kim <nickelkima@googlemail.com>
2021-06-04 20:30:47 +0200

    UserPhase

--- Implementierung von Anwendungssystemen/Views/LoginPage.xaml.cs
+++ Implementierung von Anwendungssystemen/Views/LoginPage.xaml.cs
@@ -6,16 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Implementierung_von_Anwendungssystemen.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        DBAccess dBAccess = new DBAccess();
         public LoginPage()
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
         }
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            string userName = EntryUserName.Text;
+            string userPassword = EntryUserPassword.Text;
+            string userUniversity = EntryUserUniversity.Text;
+            string userEmail = EntryUserEmail.Text;
+            if (userName.Equals(""))
+            {
+                Console.WriteLine("Bitte füge einen Namen ein");
+            }
+            else
+            {
+            }
+        }
+
     }
 }
