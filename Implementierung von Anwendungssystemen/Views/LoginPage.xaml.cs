2a02234daa91b3d4fb910ffac4908def59bd6d09
Kim <nickelkima@googlemail.com>
2021-06-05 15:22:44 +0200

    USer4

--- Implementierung von Anwendungssystemen/Views/LoginPage.xaml.cs
+++ Implementierung von Anwendungssystemen/Views/LoginPage.xaml.cs
@@ -14,63 +14,14 @@ namespace Implementierung_von_Anwendungssystemen.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
-        DBAccess objDBAccess = new DBAccess();
         public LoginPage()
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
         }
-        void Button_Clicked(object sender, EventArgs e)
+        private void Button_Clicked_1(object sender, EventArgs e)
         {
-            string userName = EntryUserName.Text;
-            string userPassword = EntryUserPassword.Text;
-            string userUniversity = EntryUserUniversity.Text;
-            string userEmail = EntryUserEmail.Text;
-            if (string.IsNullOrEmpty(userName))
-            {
-                Console.WriteLine("Bitte füge einen Namen ein");
-                DisplayAlert("Error", "Bitte füge einen Namen ein", "OK");
-            }
-            else if (string.IsNullOrEmpty(userPassword))
-            {
-                Console.WriteLine("Bitte füge ein Password ein");
-                DisplayAlert("Error", "Bitte füge ein Passwort ein", "OK");
-            }
-            else if (string.IsNullOrEmpty(userEmail))
-            {
-
-                Console.WriteLine("Bitte füge eine E-Mail ein");
-                DisplayAlert("Error", "Bitte füge eine E-Mail Adresse ein", "OK");
-            }
-            else if (string.IsNullOrEmpty(userUniversity))
-            {
-                Console.WriteLine("Bitte füge eine Universität ein");
-                DisplayAlert("Error", "Bitte füge eine Universität ein", "OK");
-            }
-
-            else
-            {
-                SqlCommand insertCommand = new SqlCommand("insert into Users(Name,Email,Password,University) values(@userName, @userEmail, @userPassword,@UserUniversity)");
-
-                /*This Part is to make the Data private*/
-                insertCommand.Parameters.AddWithValue("@userName", userName);
-                insertCommand.Parameters.AddWithValue("@userEmail", userEmail);
-                insertCommand.Parameters.AddWithValue("@userPassword", userPassword);
-                insertCommand.Parameters.AddWithValue("@userUniversity", userUniversity);
-
-                int row = objDBAccess.ExecuteQuery(insertCommand);
-                if (row == 1)
-                {
-                    DisplayAlert("Erfolgreich registriert", "Der Account wurde erfolgreich erstellt", "Weiter zum Login");
-                    Navigation.PushAsync(new AboutPage());
-                }
-                else
-                {
-                    DisplayAlert("Error", "Es wurde kein Account erstellt", "OK");
-                }
-            }
-
+            Navigation.PushAsync(new Registration());
         }
-
     }
-}
+ }
---
+++ Implementierung von Anwendungssystemen/Views/Registration.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Implementierung_von_Anwendungssystemen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registration : ContentPage
+    {
+        DBAccess objDBAccess = new DBAccess();
+        public Registration()
+        {
+            InitializeComponent();
+
+        }
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            string userName = EntryUserName.Text;
+            string userPassword = EntryUserPassword.Text;
+            string userUniversity = EntryUserUniversity.Text;
+            string userEmail = EntryUserEmail.Text;
+            if (string.IsNullOrEmpty(userName))
+            {
+                Console.WriteLine("Bitte füge einen Namen ein");
+                DisplayAlert("Error", "Bitte füge einen Namen ein", "OK");
+            }
+            else if (string.IsNullOrEmpty(userPassword))
+            {
+                Console.WriteLine("Bitte füge ein Password ein");
+                DisplayAlert("Error", "Bitte füge ein Passwort ein", "OK");
+            }
+            else if (string.IsNullOrEmpty(userEmail))
+            {
+
+                Console.WriteLine("Bitte füge eine E-Mail ein");
+                DisplayAlert("Error", "Bitte füge eine E-Mail Adresse ein", "OK");
+            }
+            else if (string.IsNullOrEmpty(userUniversity))
+            {
+                Console.WriteLine("Bitte füge eine Universität ein");
+                DisplayAlert("Error", "Bitte füge eine Universität ein", "OK");
+            }
+
+            else
+            {
+                SqlCommand insertCommand = new SqlCommand("insert into Users(Name,Email,Password,University) values(@userName, @userEmail, @userPassword,@UserUniversity)");
+
+                /*This Part is to make the Data private*/
+                insertCommand.Parameters.AddWithValue("@userName", userName);
+                insertCommand.Parameters.AddWithValue("@userEmail", userEmail);
+                insertCommand.Parameters.AddWithValue("@userPassword", userPassword);
+                insertCommand.Parameters.AddWithValue("@userUniversity", userUniversity);
+
+                int row = objDBAccess.ExecuteQuery(insertCommand);
+                if (row == 1)
+                {
+                    DisplayAlert("Erfolgreich registriert", "Der Account wurde erfolgreich erstellt", "Weiter zum Login");
+                    Navigation.PushAsync(new LoginPage());
+                }
+                else
+                {
+                    DisplayAlert("Error", "Es wurde kein Account erstellt", "OK");
+                }
+            }
+
+        }
+    }
+}
