6ec776c3e25e35c5d241d5d94c22bad6dac2c7f1
Kim <nickelkima@googlemail.com>
2021-07-21 11:59:45 +0200

    Manual Distance part1

---
+++ Implementierung von Anwendungssystemen/Views/ManualDistance.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace Implementierung_von_Anwendungssystemen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManualDistance : ContentPage
+    {
+
+        public ManualDistance()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AddDistance_Clicked(object sender, EventArgs e)
+        {
+            float manualDistance;
+            string manualTypeOfSport = EntryManualTypeOfSport.Text;
+            string manualDayTime = EntryManualTimeOfTheDay.Text;
+            double manualDuration;
+            float manualAverageSpeed;
+            int manualCaloriesBurned;
+
+            float value0;
+            string stringManualDistance = EntryManualDistance.Text;
+            if (float.TryParse(stringManualDistance, out value0))
+            {
+                manualDistance = float.Parse(stringManualDistance);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please insert the distance only as numbers", "OK");
+            }
+            double value1;
+            string stringManualDuration = EntryManualDuration.Text;
+            if (double.TryParse(stringManualDuration, out value1))
+            {
+                manualDuration = double.Parse(stringManualDuration);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please insert the duration only as numbers", "OK");
+            }
+
+            float value2;
+            string stringManualAverageSpeed = EntryManualAverageSpeed.Text;
+            if (float.TryParse(stringManualDuration, out value2))
+            {
+                manualAverageSpeed = float.Parse(stringManualDuration);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please insert the the average speed only as numbers", "OK");
+            }
+
+            int value3;
+            string stringManualCaloriesBurned = EntryManualCaloriesBurned.Text;
+            if (int.TryParse(stringManualCaloriesBurned, out value3))
+            {
+                manualCaloriesBurned = int.Parse(stringManualDuration);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please insert the the calories burned only as numbers", "OK");
+            }
+
+
+        }
+    }
+}
