7d5bfb239fa4eb3a9111785c3db15dcce3545cfe
Marc <marcleon.hohmann@web.de>
2021-07-22 23:49:51 +0200

    Neues Logo

--- Implementierung von Anwendungssystemen.Android/MainActivity.cs
+++ Implementierung von Anwendungssystemen.Android/MainActivity.cs
@@ -17,7 +17,7 @@ using Android.Content;
 
 namespace Implementierung_von_Anwendungssystemen.Droid
 {
-    [Activity(Label = "Implementierung_von_Anwendungssystemen", Icon = "@mipmap/Logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Implementierung_von_Anwendungssystemen", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
